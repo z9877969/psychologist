6eccfc7786496bb5ec2620fb5c7460867724ba0a
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-03-19 15:52:20 +0200

    create ConsultationBook component

---
+++ src/modules/_template/components/ConsultationBook/ConsultationBook.jsx
@@ -0,0 +1,7 @@
+import s from './ConsultationBook.module.scss';
+
+const ConsultationBook = () => {
+  return <div className={s.wrapp}>ConsultationBook</div>;
+};
+
+export default ConsultationBook;
