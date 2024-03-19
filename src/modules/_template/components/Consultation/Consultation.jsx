2a1b3a960f43658582aa041d4968d6b7e4682054
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-03-19 21:39:44 +0200

    media  + style

--- src/modules/_template/components/Consultation/Consultation.jsx
+++ src/modules/_template/components/Consultation/Consultation.jsx
@@ -10,44 +10,57 @@ import {
   consultationTab2x,
 } from 'modules/_template/images/consultation';
 
+const Problems = ({ arrey }) => {
+  return (
+    <ul className={s.problems}>
+      {arrey.map(({ id, text }) => {
+        return (
+          <li className={s.problem} key={id}>
+            <span className={s.id}>{id}.</span>
+            <span className={s.itemText}>{text}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
 const Consultation = () => {
   return (
     <section className={s.section}>
       <Container>
-        <div className={s.thumb}>
-          <picture>
-            <source
-              media="(min-width: 1444px)"
-              srcSet={`${consultationDesc} 1x, ${consultationDesc2x} 2x`}
-            />
-            <source
-              media="(min-width: 768px)"
-              srcSet={`${consultationTab} 1x, ${consultationTab2x} 2x`}
-            />
-            <img
-              className={s.img}
-              srcSet={`${consultationMob} 1x, ${consultationMob2x} 2x`}
-              src={consultationMob}
-              alt="psyshologist avatar"
-            />
-          </picture>
-        </div>
-        <div className={s.content}>
-          <p className={s.title}>Проблеми, з якими я працюю</p>
-          <p className={s.text}>
-            Під час зустрічі ви зможете розповісти, що спонукало вас звернутися,
-            а я поділюся з вами своїм баченням нашої майбутньої роботи.
-          </p>
-          <ul className={s.problems}>
-            {problems.map(({ id, text }) => {
-              return (
-                <li className={s.problem} key={id}>
-                  <span className={s.id}>{id}.</span>
-                  <span className={s.itemText}>{text}</span>
-                </li>
-              );
-            })}
-          </ul>
+        <div className={s.consultationInfo}>
+          <div className={s.thumb}>
+            <picture>
+              <source
+                media="(min-width: 1444px)"
+                srcSet={`${consultationDesc} 1x, ${consultationDesc2x} 2x`}
+              />
+              <source
+                media="(min-width: 768px)"
+                srcSet={`${consultationTab} 1x, ${consultationTab2x} 2x`}
+              />
+              <img
+                className={s.img}
+                srcSet={`${consultationMob} 1x, ${consultationMob2x} 2x`}
+                src={consultationMob}
+                alt="psyshologist avatar"
+                width={343}
+              />
+            </picture>
+          </div>
+          <div className={s.content}>
+            <p className={s.title}>Проблеми, з якими я працюю</p>
+            <p className={s.text}>
+              Під час зустрічі ви зможете розповісти, що спонукало вас
+              звернутися, а я поділюся з вами своїм баченням нашої майбутньої
+              роботи.
+            </p>
+            <div className={s.problemsWrapper}>
+              <Problems arrey={problems.slice(0, 4)} />
+              <Problems arrey={problems.slice(4)} />
+            </div>
+          </div>
         </div>
       </Container>
     </section>
