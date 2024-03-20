217f09bddd69776507958ce3bf002a225cff884b
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-03-20 15:19:15 +0200

    fix mistakes in FAQ
    Видалив зайві файли стилів і змінив використання Імж на готовий компонент пікчер

--- src/modules/frequentlyAskedQuestions/components/FrequentlyAskedQuestions/FrequentlyAskedQuestions.jsx
+++ src/modules/frequentlyAskedQuestions/components/FrequentlyAskedQuestions/FrequentlyAskedQuestions.jsx
@@ -5,14 +5,15 @@ import imgTablet from '../../img/photoFAQtablet.jpg';
 import imgDesk from '../../img/photoFAQdesk.jpg';
 import ScrollBar from '../ScrollBar/ScrollBar';
 import FAQList from '../FAQList/FAQList';
+import Picture from 'shared/components/PIcture/Picture';
 
 const getSrc = () => {
   const width = parseInt(
     getComputedStyle(document.querySelector('body')).width
   );
-  if (width < 768) return imgMobail;
-  if (width < 1440) return imgTablet;
-  return imgDesk;
+  if (width < 768) return [343, 343];
+  if (width < 1440) return [688, 532];
+  return [708, 708];
 };
 
 const FrequentlyAskedQuestions = () => {
@@ -21,8 +22,18 @@ const FrequentlyAskedQuestions = () => {
       <Container>
         <div className={s.container}>
           <div className={s.imgContainer}>
-            <img src={getSrc()} alt="" />
+            <Picture
+              urlMobile={imgMobail}
+              urlMobile2x={imgMobail}
+              urlTablet={imgTablet}
+              urlTablet2x={imgTablet}
+              urlDesktop={imgDesk}
+              urlDesktop2x={imgDesk}
+              width={getSrc()[0]}
+              height={getSrc()[1]}
+            />
           </div>
+
           <div>
             <h2 className={s.title}>FAQ</h2>
             <ScrollBar>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,13 @@
 import { ModuleTemplate } from 'modules/_template';
 import { Blog } from 'modules/blogSection';
+import FrequentlyAskedQuestions from 'modules/frequentlyAskedQuestions/components/FrequentlyAskedQuestions/FrequentlyAskedQuestions';
 
 const MainPage = () => {
   return (
     <>
       <Blog />
       <ModuleTemplate />
+      <FrequentlyAskedQuestions />
     </>
   );
 };
