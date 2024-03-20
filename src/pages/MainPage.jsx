5b029a149d300ea5cad49c40e1218d4501f3a979
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-03-20 17:52:56 +0200

    Update MainPage.jsx
    вирішив конфлікт

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,11 @@
 import { ModuleTemplate } from 'modules/_template';
 import { Blog } from 'modules/blogSection';
-import FrequentlyAskedQuestions from 'modules/frequentlyAskedQuestions/components/FrequentlyAskedQuestions/FrequentlyAskedQuestions';
 
 const MainPage = () => {
   return (
     <>
       <Blog />
       <ModuleTemplate />
-      <FrequentlyAskedQuestions />
     </>
   );
 };
