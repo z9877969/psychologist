13568029431a37bc0572f2dd57d291e16bd584f8
IrinaLender27 <irina.lender27@gmail.com>
2024-03-20 22:17:08 +0200

    update header

--- src/modules/header/components/Header/Header.jsx
+++ src/modules/header/components/Header/Header.jsx
@@ -1,13 +1,31 @@
 import { Container } from 'shared/components';
 import Navigation from '../Navigation/Navigation';
+import Logo from '../Logo/Logo';
+import { Link } from 'react-router-dom';
+import s from './Header.module.scss';
+import { useState } from 'react';
+import Phone from '../Phone/Phone';
+import BurgerMenu from '../BurgerMenu/BurgerMenu';
+import MobileMenu from '../MobileMenu/MobileMenu';
 
 const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
   return (
-    <header className="h1">
+    <header>
       <Container>
-        Header
-        <Navigation />
+        <div className={s.headerWrapper}>
+          <BurgerMenu isOpen={isMobileMenuOpen} onClick={toggleMobileMenu} />
+          <Link to={'/'}>
+            <Logo />
+          </Link>
+          <Navigation />
+          <Phone />
+        </div>
       </Container>
+      <MobileMenu isOpen={isMobileMenuOpen} onClose={toggleMobileMenu} />
     </header>
   );
 };
