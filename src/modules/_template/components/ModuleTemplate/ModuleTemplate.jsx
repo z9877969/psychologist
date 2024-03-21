72354fef930dc37a8af655178596991c2d87008d
Dtimewarp <dtimewarp@gmail.com>
2024-03-21 12:23:11 +0200

    upd component PopUp

--- src/modules/_template/components/ModuleTemplate/ModuleTemplate.jsx
+++ src/modules/_template/components/ModuleTemplate/ModuleTemplate.jsx
@@ -1,12 +1,14 @@
 import { Container } from 'shared/components';
 import SubComponent from '../SubComponent/SubComponent';
 import s from './ModuleTemplate.module.scss';
+import { SignUpPopUp } from '../SignUpPopUp/SignUpPopUp';
 
 const ModuleTemplate = () => {
   return (
     <section className={s.section}>
       <Container>
         <SubComponent />
+        <SignUpPopUp />
       </Container>
     </section>
   );
--- src/modules/_template/components/SubComponent/SubComponent.jsx
+++ src/modules/_template/components/SubComponent/SubComponent.jsx
@@ -1,13 +1,13 @@
 import s from './SubComponent.module.scss';
 import { abductors } from '../../images';
-import { SignUpPopUp } from '../PopUp/SignUpPopUp';
+// import { SignUpPopUp } from '../SignUpPopUp/SignUpPopUp';
 
 const SubComponent = () => {
   return (
     <>
       <h2 className={s.title}>SubComponent</h2>
       <img src={abductors} alt="some image description" />
-      <SignUpPopUp />
+      {/* <SignUpPopUp /> */}
     </>
   );
 };
