667af9079197b13559ec8e9d725766af65b6791b
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-03-22 20:31:59 +0200

    add Navlink

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,13 +1,25 @@
 import articles from '../modules/blogPage/data/articles.json';
-import BackToArticlesBtn from '../modules/blogPage/components/BackToArticlesBtn/BackToArticlesBtn';
+
 import ContentList from 'modules/blogPage/components/ContentList/ContentList';
 import { Container } from 'shared/components';
+import { NavLink } from 'react-router-dom';
+import { sprite } from 'shared/icons';
 
 const BlogPage = () => {
   return (
     <Container>
       <div>
-        <BackToArticlesBtn />
+        <NavLink
+          to="/blog"
+          style={{ display: 'flex', alignItems: 'center', color: 'black' }}
+        >
+          <svg width="24" height="24" style={{ marginRight: '5px' }}>
+            <use xlinkHref={`${sprite}#icon-paginator-chevron-left`}></use>
+          </svg>
+          Повернутися до всіх статтей
+        </NavLink>
+      </div>
+      <div>
         <ContentList articles={articles} />
       </div>
     </Container>
