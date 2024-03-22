ef66197822ba7e236a15be4abf775d5d1ef8a136
Vasyl Diachuk <djachukvasja@gmail.com>
2024-03-22 13:16:10 +0200

    112233

--- src/shared/components/BlogTitle/BlogTitle.jsx
+++ src/shared/components/BlogTitle/BlogTitle.jsx
@@ -1,9 +1,9 @@
 import s from './BlogTitle.module.scss';
 
-const BlogTitle = ({ className }) => {
+const BlogTitle = () => {
   return (
     <>
-      <h2 className={className}>Блог</h2>
+      <h2 className={s.title}>Блог</h2>
       <p className={s.description}>
         Розмірковую над темами, які мене зацікавили. Запрошую читачів разом зі
         мною досліджувати ключові поняття психології
