f7b75f89184a9fa04a493fe0510afbc564a8944f
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-03-22 09:39:21 +0200

    add components

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,18 +1,10 @@
-// import { FirstParagraph } from 'modules/_template/components/OpenedBlog/FirstParagraph/FirstParagraph';
-// import { Image } from 'modules/_template/components/OpenedBlog/Image/Image';
-// import { ParagraphAboutMe } from 'modules/_template/components/OpenedBlog/ParagraphAboutMe/ParagraphAboutMe';
-// import { Quote } from 'modules/_template/components/OpenedBlog/Quote/Quote';
-// import { SecondParagraph } from 'modules/_template/components/OpenedBlog/SecondParagraph/SecondParagraph';
-// import { useRef } from 'react';
-// import { Link, useLocation } from 'react-router-dom/dist';
-
 // const location = useLocation();
 // const backLinkRef = useRef(location).state;
 
-import ContentList from 'modules/_template/components/OpenedBlog/ContentList/ContentList';
-import articles from '../modules/_template/data/articles.json';
-import lastArticles from '../modules/_template/data/lastArticles.json';
-import LastArticlesList from 'modules/_template/components/OpenedBlog/LastArticlesList/LastArticlesList';
+import ContentList from 'modules/OpenBlog/components/ContentList/ContentList';
+import articles from '../modules/OpenBlog/data/articles.json';
+import lastArticles from '../modules/OpenBlog/data/lastArticles.json';
+import LastArticlesList from 'modules/OpenBlog/components/LastArticlesList/LastArticlesList';
 import { Container } from 'shared/components';
 import { NavLink } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
