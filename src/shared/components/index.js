4805069e09380b9d5ac890831c7940cf08179668
Antony Ishchenko <antony.ishchenko@gmail.com>
2024-03-24 20:59:26 +0200

    blog-list-fix

--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -3,3 +3,6 @@ export { default as SharedLayout } from './SharedLayout/SharedLayout';
 export { default as Picture } from './Picture/Picture';
 export { default as ScrollUpButton } from './ScrollUpButton/ScrollUpButton';
 export { default as ModalBackdrop } from './ModalBackdrop/ModalBackdrop';
+
+export { default as BlogListHeader } from './BlogListHeader/BlogListHeader';
+export { default as Section } from './Section/Section';
