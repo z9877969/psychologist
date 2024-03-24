8295e77c13afdefc75dabeaafe37e35a816a1af8
Yevhenii Pozniak <jekamanu@gmail.com>
2024-03-24 20:49:07 +0200

    videoSection and aboutSection

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,15 @@
 import { ModuleTemplate } from 'modules/_template';
+import About from 'modules/aboutSection/components/About/About';
 import { Blog } from 'modules/blogSection';
+import VideoSection from 'modules/videoSection/components/VideoSection/VideoSection';
 
 const MainPage = () => {
   return (
     <>
+      <VideoSection />
+      <About />
       <Blog />
+
       <ModuleTemplate />
     </>
   );
