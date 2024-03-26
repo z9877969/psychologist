ffb2672531b8f2a68085baa1684f3ccfe2c28f8b
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-03-26 10:40:34 +0000

    buttons

--- src/modules/swiper/components/SliderNavButtons/SliderNavButtons.jsx
+++ src/modules/swiper/components/SliderNavButtons/SliderNavButtons.jsx
@@ -1,4 +1,23 @@
+import { sprite } from 'shared/icons';
+import s from './SliderNavButtons.module.scss';
+import clsx from 'clsx';
+
 export const SliderNavButtons = ({ swiperRef }) => {
+  const hasPrevSlide = true;
+  const hasNextSlides = true;
+
+  const renderIcon = (id, isActive) => {
+    return (
+      <svg
+        className={clsx(s.svg, !isActive && s.buttonDisab)}
+        width="48"
+        height="48"
+      >
+        <use href={`${sprite}#${id}`}></use>
+      </svg>
+    );
+  };
+
   const handlePrev = () => {
     swiperRef.current && swiperRef.current.slidePrev();
   };
@@ -8,12 +27,13 @@ export const SliderNavButtons = ({ swiperRef }) => {
   };
 
   return (
-    <div
-      className="swiper-nav-btns"
-      style={{ position: 'absolute', top: '-40px', right: '0' }}
-    >
-      <button onClick={handlePrev}>Prev</button>
-      <button onClick={handleNext}>Next</button>
+    <div className={s.swiper_nav_btns}>
+      <button onClick={handlePrev} className={s.button_arrow}>
+        {renderIcon('icon-arrows-left', hasPrevSlide)}
+      </button>
+      <button onClick={handleNext} className={s.button_arrow}>
+        {renderIcon('icon-arrows-right', hasNextSlides)}
+      </button>
     </div>
   );
 };
