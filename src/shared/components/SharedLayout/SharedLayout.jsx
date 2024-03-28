2486075898bd82b008b12b5158bde5c8c1f6c0ec
z9877969 <z9877969@ukr.net>
2024-03-28 23:27:19 +0200

    adds codespliting and page stable height

--- src/shared/components/SharedLayout/SharedLayout.jsx
+++ src/shared/components/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,18 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 import { Footer } from 'modules/footer';
 import { Header } from 'modules/header';
-// import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
 import { ScrollUpButton } from '..';
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
-      {/* <Suspense fallback={null}> */}
-      <Outlet />
-      {/* </Suspense> */}
+      <div style={{ minHeight: '100vh' }}>
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </div>
       <ScrollUpButton />
       <Footer />
     </>
