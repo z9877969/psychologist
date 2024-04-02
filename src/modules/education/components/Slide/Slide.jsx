9655c3e0387c2ec901a3fd9a2b2b3d9246cbcc65
Antony Ishchenko <antony.ishchenko@gmail.com>
2024-04-02 23:34:51 +0300

    fix

--- src/modules/education/components/Slide/Slide.jsx
+++ src/modules/education/components/Slide/Slide.jsx
@@ -1,4 +1,5 @@
 import { Picture } from 'shared/components';
+import { useCallback } from 'react';
 import s from './Slide.module.scss';
 import { useModal } from 'context/ModalProvider';
 import Modal from '../Modal/Modal';
@@ -6,16 +7,17 @@ import Modal from '../Modal/Modal';
 export default function Slide({ text, ...props }) {
   const setModal = useModal();
 
-  const handleClick = () => setModal(<Modal {...props} />);
+  const handleClick = useCallback(
+    () => setModal(<Modal {...props} />),
+    [setModal, props]
+  );
 
   return (
     <div className={s.slide} onClick={handleClick}>
       <div className={s.frame}>
         <Picture {...props} className={s.picture} />
       </div>
-      <button className={s.sliderText} onClick={handleClick}>
-        {text}
-      </button>
+      <button className={s.sliderText}>{text}</button>
     </div>
   );
 }
