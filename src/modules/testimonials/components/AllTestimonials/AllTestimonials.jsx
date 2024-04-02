4e3c2c6ba8d72a81dbae5c4fc79a098103f586aa
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-04-01 21:14:15 -0400

    fix

--- src/modules/testimonials/components/AllTestimonials/AllTestimonials.jsx
+++ src/modules/testimonials/components/AllTestimonials/AllTestimonials.jsx
@@ -1,24 +1,43 @@
 import ScrollBar from '../ScrollBar/ScrollBar';
 import TestimonialCard from '../TestimonialCard/TestimonialCard';
+import ModalBackdrop from 'shared/components/ModalBackdrop/ModalBackdrop';
+
 import s from './AllTestimonials.module.scss';
+import { sprite } from 'shared/icons';
 
 const AllTestimonials = ({ testimonials, onClose }) => {
+  const cardStyles = {
+    text: s.textInModal,
+    quote: s.quoteInModal,
+    footer: s.footerInModal,
+  };
   return (
-    <div className={s.testimonialsModal}>
-      <div className={s.modalHeader}>
-        <h2>Відгуки моїх клієнітів</h2>
-        <button onClick={onClose} className={s.closeButton}>
-          ×
-        </button>
-      </div>
-      <ScrollBar>
-        <div className={s.cardsContainer}>
-          {testimonials.map((testimonial) => (
-            <TestimonialCard key={testimonial.id} {...testimonial} />
-          ))}
+    <ModalBackdrop onClick={onClose}>
+      <div className={s.testimonialsModal} onClick={(e) => e.stopPropagation()}>
+        <div className={s.modalHeader}>
+          <h2>Відгуки моїх клієнітів</h2>
+          <div>
+            <button className={s.closeButton} onClick={onClose}>
+              <svg className={s.icon}>
+                <use xlinkHref={`${sprite}#icon-cross`}></use>
+              </svg>
+            </button>
+          </div>
         </div>
-      </ScrollBar>
-    </div>
+        <ScrollBar>
+          <div className={s.cardsContainer}>
+            {testimonials.map((testimonial) => (
+              <TestimonialCard
+                key={testimonial.id}
+                {...testimonial}
+                className={s.testimonialCardInModal}
+                styles={cardStyles}
+              />
+            ))}
+          </div>
+        </ScrollBar>
+      </div>
+    </ModalBackdrop>
   );
 };
 
--- src/modules/testimonials/components/TestimonialCard/TestimonialCard.jsx
+++ src/modules/testimonials/components/TestimonialCard/TestimonialCard.jsx
@@ -1,12 +1,17 @@
 import s from './TestimonialCard.module.scss';
 import { formatAge } from '../../utils/formatAge';
+import { sprite } from 'shared/icons';
 
-function TestimonialCard({ text, age, author }) {
+function TestimonialCard({ text, age, author, className, styles }) {
   return (
-    <div className={s.card}>
-      <p className={s.quote}>“</p>
-      <p className={s.text}>{`"${text}"`}</p>
-      <p className={s.footer}>
+    <div className={className ? className : s.card}>
+      <svg className={styles && styles.quote ? styles.quote : s.quote}>
+        <use xlinkHref={`${sprite}#icon-quote`}></use>
+      </svg>
+      <p
+        className={styles && styles.text ? styles.text : s.text}
+      >{`"${text}"`}</p>
+      <p className={styles && styles.footer ? styles.footer : s.footer}>
         {author}, {formatAge(age)}
       </p>
     </div>
