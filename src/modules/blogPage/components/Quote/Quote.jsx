69ee736d1bb242070c10f08d88f97d57cc641210
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-04-02 20:21:51 +0300

    add svg

--- src/modules/blogPage/components/Quote/Quote.jsx
+++ src/modules/blogPage/components/Quote/Quote.jsx
@@ -1,10 +1,13 @@
+import { sprite } from 'shared/icons';
 import s from './Quote.module.scss';
 
 export const Quote = ({ content, accent, author }) => {
   return (
     <section className={s.wraperQuote}>
       <div className={s.quote}>
-        <p className={s.textEl}>“</p>
+        <svg width={42} height={42}>
+          <use href={`${sprite}#icon-element-text-right`}></use>
+        </svg>
         <div className={s.wrapContent}>
           <p className={s.content}>
             <span className={s.accent}>{accent}</span>
@@ -12,7 +15,9 @@ export const Quote = ({ content, accent, author }) => {
           </p>
         </div>
         <div className={s.wrapEl}>
-          <p className={s.textEl}>”</p>
+          <svg width={42} height={42}>
+            <use href={`${sprite}#icon-element-text-left`}></use>
+          </svg>
         </div>
       </div>
       <div className={s.wrapAuthor}>
