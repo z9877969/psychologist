2f4e3cd4ab4e3236385e49732421ceeccc086a9d
Vasyl Diachuk <djachukvasja@gmail.com>
2024-04-03 17:15:12 +0300

    done

--- src/shared/components/ScrollUpButton/ScrollUpButton.jsx
+++ src/shared/components/ScrollUpButton/ScrollUpButton.jsx
@@ -2,15 +2,18 @@ import { useEffect, useState } from 'react';
 import s from './ScrollUpButton.module.scss';
 import { sprite } from 'shared/icons';
 import clsx from 'clsx';
-import { useMediaQuery } from 'hooks/index';
+import { useMediaQuery } from 'react-responsive';
 
 const ScrollUpButton = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const media = useMediaQuery();
+
+  const isBigDesktop = useMediaQuery({ query: '(min-width: 1590px)' });
+  const isTablet = useMediaQuery({ query: '(min-width: 768px)' });
+  const isDesktop = useMediaQuery({ query: '(min-width: 1440px)' });
 
   let startPosition = 680;
-  if (media.isTablet) startPosition = 1016;
-  if (media.isDesktop) startPosition = 736;
+  if (isTablet) startPosition = 1016;
+  if (isDesktop) startPosition = 736;
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -22,12 +25,22 @@ const ScrollUpButton = () => {
     };
   }, [startPosition]);
 
+  const getPosition = () => {
+    if (!isBigDesktop) return {};
+    const width = document.body.clientWidth;
+    const transform = (width - 1440) / 4 + 1412;
+    return {
+      transform: `translate(${transform}px, 0px)`,
+    };
+  };
+
   return (
     <div className={s.wrapper}>
       <button
         className={clsx(s.button, isOpen && s.isOpen)}
         onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
         type="button"
+        style={getPosition()}
       >
         <svg className={s.svg} width="24" height="24">
           <use xlinkHref={`${sprite}#icon-scroll-arrow`}></use>
