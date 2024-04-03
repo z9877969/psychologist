b41913b4fb6bcdd0cdb0b81eae116a0c5572a090
Vasyl Diachuk <djachukvasja@gmail.com>
2024-04-03 14:03:15 +0300

    1221

--- src/modules/testimonials/components/TestimonialCard/TestimonialCard.jsx
+++ src/modules/testimonials/components/TestimonialCard/TestimonialCard.jsx
@@ -1,11 +1,14 @@
 import s from './TestimonialCard.module.scss';
 import { formatAge } from '../../utils/formatAge';
+import clsx from 'clsx';
 
-function TestimonialCard({ text, age, author }) {
+function TestimonialCard({ text, age, author, isLimitation = true }) {
   return (
-    <div className={s.card}>
-      <p className={s.quote}>“</p>
-      <p className={s.text}>{`"${text}"`}</p>
+    <div className={clsx(s.card, isLimitation && s.cardLimit)}>
+      {/* <p className={s.quote}>“</p> */}
+
+      <p className={clsx(s.text, isLimitation && s.limit)}>{`"${text}"`}</p>
+
       <p className={s.footer}>
         {author}, {formatAge(age)}
       </p>
