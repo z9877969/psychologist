0134a9803e016d38c69833137c38dbbab0aed0eb
IrinaLender27 <irina.lender27@gmail.com>
2024-04-04 10:47:52 +0300

    add info

--- src/modules/TeamModal/DeveloperCard/DeveloperCard.jsx
+++ src/modules/TeamModal/DeveloperCard/DeveloperCard.jsx
@@ -1,20 +1,25 @@
 import { Link } from 'react-router-dom';
 import s from './DeveloperCard.module.scss';
 import { sprite } from 'shared/icons';
+import * as images from '../img';
 
 const DeveloperCard = ({ developer }) => {
-  const { name, position, photo, linkedin } = developer;
-  const photoSrc = photo || 'https://via.placeholder.com/80';
+  const { name, position, img1x, img2x, linkedin } = developer;
+
   return (
     <div className={s.card}>
       <Link to={linkedin} target="_blank" className={s.link}>
         <div className={s.container}>
-          <img className={s.photo} src={photoSrc} alt={name} />
+          <img
+            className={s.photo}
+            src={images[img1x]}
+            srcSet={`${images[img1x]} 1x, ${images[img2x]} 2x`}
+            alt={name}
+          />
           <svg className={s.icon}>
             <use xlinkHref={`${sprite}#icon-linkedin`}></use>
           </svg>
         </div>
-
         <div className={s.details}>
           <h3 className={s.name}>{name}</h3>
           <p className={s.position}>{position}</p>
