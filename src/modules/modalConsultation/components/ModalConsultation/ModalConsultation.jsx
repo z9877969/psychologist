7a3b3564154f8c2a5fc7573f426833500046f107
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-04-12 10:04:11 +0300

    fix modal

--- src/modules/modalConsultation/components/ModalConsultation/ModalConsultation.jsx
+++ src/modules/modalConsultation/components/ModalConsultation/ModalConsultation.jsx
@@ -8,25 +8,28 @@ import { useCallback } from 'react';
 const ModalConsultation = () => {
   const setModal = useModal();
   const closeModal = useCallback(() => setModal(), [setModal]);
+
   return (
-    <div className={s.modal}>
-      <div className={s.container}>
-        <div className={s.imgContainer}>
-          <MyPicture />
+    <div style={{ paddingBottom: 40 }}>
+      <div className={s.modal}>
+        <div className={s.container}>
           <button className={s.btnClose} onClick={closeModal}>
             <svg className={s.icon} width={24} height={24}>
               <use xlinkHref={`${sprite}#icon-cross`}></use>
             </svg>
           </button>
-        </div>
-        <div className={s.formContainer}>
-          <h2 className={s.title}>Записатися на консультацію</h2>
-          <button className={s.btnSecond} onClick={() => closeModal()}>
-            <svg className={s.icon} width={24} height={24}>
-              <use xlinkHref={`${sprite}#icon-cross`}></use>
-            </svg>
-          </button>
-          <FormConsultation />
+          <div className={s.imgContainer}>
+            <MyPicture />
+          </div>
+          <div className={s.formContainer}>
+            <h2 className={s.title}>Записатися на консультацію</h2>
+            <button className={s.btnSecond} onClick={() => closeModal()}>
+              <svg className={s.icon} width={24} height={24}>
+                <use xlinkHref={`${sprite}#icon-cross`}></use>
+              </svg>
+            </button>
+            <FormConsultation />
+          </div>
         </div>
       </div>
     </div>
