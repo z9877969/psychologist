c4bf66af6d36075446d2f4d702575e461e583670
z9877969 <z9877969@ukr.net>
2024-07-05 18:58:23 +0300

    fixes format thousands for consultation price

--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -4,6 +4,7 @@ export { default as Picture } from './Picture/Picture';
 export { default as PictureContent } from './PictureContent/PictureContent';
 export { default as Section } from './Section/Section';
 export { default as Button } from './Button/Button';
+export { default as BigButton } from './BigButton/BigButton';
 export { default as ScrollUpButton } from './ScrollUpButton/ScrollUpButton';
 export { default as ModalBackdrop } from './ModalBackdrop/ModalBackdrop';
 export { default as BlogListHeader } from './BlogListHeader/BlogListHeader';
